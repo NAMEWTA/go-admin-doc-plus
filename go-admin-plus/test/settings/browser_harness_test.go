a5f915c9848e5dcbb1dd86c634d8939c5bc14b2f
wta <wta2023@foxmail.com>
2026-09-01 13:30:23 +0800

    test(e2e): route Settings browser workflows

--- go-admin-plus/test/settings/browser_harness_test.go
+++ go-admin-plus/test/settings/browser_harness_test.go
@@ -20,6 +20,7 @@ import (
 	"github.com/NAMEWTA/go-admin-plus/go-admin-plus/internal/modules/iam/authorization"
 	sessionmigration "github.com/NAMEWTA/go-admin-plus/go-admin-plus/internal/modules/iam/migrations/0010-session-schema"
 	administrationmigration "github.com/NAMEWTA/go-admin-plus/go-admin-plus/internal/modules/iam/migrations/0020-administration-schema"
+	sessionprotectionmigration "github.com/NAMEWTA/go-admin-plus/go-admin-plus/internal/modules/iam/migrations/0040-session-protection"
 	"github.com/NAMEWTA/go-admin-plus/go-admin-plus/internal/modules/iam/session"
 	"github.com/NAMEWTA/go-admin-plus/go-admin-plus/internal/modules/settings"
 	settingsmigration "github.com/NAMEWTA/go-admin-plus/go-admin-plus/internal/modules/settings/migrations/0010-settings"
@@ -67,7 +68,7 @@ func TestSettingsBrowserHarnessServer(t *testing.T) {
 				t.Fatalf("settings PostgreSQL schema isolation failed current=%q", schema)
 			}
 		}
-		runner, err := migrations.NewRunner(sessionmigration.Provider{}, administrationmigration.Provider{}, settingsmigration.Provider{})
+		runner, err := migrations.NewRunner(sessionmigration.Provider{}, administrationmigration.Provider{}, sessionprotectionmigration.Provider{}, settingsmigration.Provider{})
 		if err != nil {
 			t.Fatal(err)
 		}
